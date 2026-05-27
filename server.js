38cb6c5daf00ecc36d29dbe9e03fc2a94f6725fc
root <root@localhost.localdomain>
2026-05-28 00:33:12 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", express.static(path.join(__dirname, "admin")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Coffee Shop running on port ${PORT}`));
